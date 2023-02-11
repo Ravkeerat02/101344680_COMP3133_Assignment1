36d4f79e196cea7f8685c630b1eeabae657337b0
Ravkeerat Singh <ravkeerat.singh02@gmail.com>
2023-02-11 15:37:32 -0500

    Mutation method updated

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -24,6 +24,8 @@ module.exports = {
 // }
 
 //allow user to create new account
+
+Mutation:{
 createUser : async function({UserInput}){
     const user = new User({
         username: UserInput.username,
@@ -33,31 +35,28 @@ createUser : async function({UserInput}){
     const result = await user.save();
     return {...result._doc, _id: result.id};
 }
-}
+},
 
 //allow user to create new employee
-// createEmployee : async function({})
+createEmployee : async function({EmployeeInput}){
+    const employee = new Employee({
+        first_name: EmployeeInput.first_name,
+        last_name: EmployeeInput.last_name,
+        email: EmployeeInput.email,
+        salary: EmployeeInput.salary
+    });
+    const result = await employee.save();
+    return {...result._doc, _id: result.id};
+},
 
 //allow user to update employee
-updateEmployee : async function({EmployeeInput}){
-    const employee = new Employee({
-        first_name: EmployeeInput.first_name,
-        last_name: EmployeeInput.last_name,
-        email: EmployeeInput.email,
-        salary: EmployeeInput.salary
-    });
-    const result = await employee.save();
-    return {...result._doc, _id: result.id};
-}
+async updateEmployee(_,{ID, EmployeeInput:{first_name, last_name, email, salary}}){
+    const wasUpdated = (await Employee.updateOne({_id: ID}, {$set: {first_name, last_name, email, salary}})).modifiedCount;
+    return wasUpdated;
+},
 
 //allow user to delete employee
-deleteEmployee : async function({EmployeeInput}){
-    const employee = new Employee({
-        first_name: EmployeeInput.first_name,
-        last_name: EmployeeInput.last_name,
-        email: EmployeeInput.email,
-        salary: EmployeeInput.salary
-    });
-    const result = await employee.save();
-    return {...result._doc, _id: result.id};
+async deleteEmployee(_,{ID}){await Employee.deleteOne({_id: ID}).deleteCount;
+    return wasDeleted;
+}
 }
