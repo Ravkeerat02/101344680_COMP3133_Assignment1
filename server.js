fd7d1f8b55d8a86baf5b9a2421fe20d3a68cb74b
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-02-12 02:05:24 -0500

    Mutation complete

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 //Apollo server
-const {typeDefs} = require('./graphql/typeDefs');
-const {resolvers} = require('./graphql/resolver');
+const typeDefs= require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const Server = new ApolloServer({
     typeDefs
     ,resolvers
 });
 
-const SERVER_PORT = 4000 //port numberer initialized
+const SERVER_PORT = 5000 //port numberer initialized
 
 //DB connection string
-const DB_URL = "mongodb+srv://RK_02:Semester23@cluster0.iu4uasl.mongodb.net/comp3133_assignmnet1?retryWrites=true&w=majority"
+const DB_URL = "mongodb+srv://RK_02:ab8UjMGR44roYdJH@cluster0.iu4uasl.mongodb.net/comp3133_assignmnet1?retryWrites=true&w=majority"
 
 const app = express();
 
@@ -24,18 +24,11 @@ app.use(bodyParser.json())
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("Successfully connected to the database mongoDB Atlas Server");    
-}).catch(err => {
-    console.log('Could not connect to the database. Exiting now...', err);
-    process.exit();
-});
-
-
-//Refers to the port beign used 
-app.listen(SERVER_PORT, () =>{
-    console.log(`Server running at http://localhost:${SERVER_PORT}/`)
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log("Successfully connected to the database");
+    return Server.listen({port: SERVER_PORT});    
 })
+.then((res) => {
+    console.log(`Server running at ${res.url}`);
+});
