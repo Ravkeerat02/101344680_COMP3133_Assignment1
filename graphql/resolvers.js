10b5bb56fd58d705c9d48a0650c7db751f938ce3
Ravkeerat02 <ravkeerat.singh02@gmail.com>
2023-02-14 03:45:26 -0500

    small fixes

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,5 @@
 const employee = require('../model/employee');
 const user = require('../model/user');
-const users = require('../model/user');
 
 module.exports = {
 
@@ -8,27 +7,34 @@ module.exports = {
 //allow user to create new account
 
 Mutation:{
-    async createUser(_,{UserInput:{username, email, password}}){
-        const user = new users({
-            username,
-            email,
-            password
-        })
-        const newUser = await user.save();
-        return {
-            id:res.id,
-            ...res.doc
-        }
-    },
+    async createUser(_,{UserInput:{username, password,email}}){
+        const User = new user({
+        username : username,
+        password  :password,
+        email : email
+    })
+    const res = await createUser.save();
+    return {
+        id: res.id,
+        ...res.doc, //showing different fields of user
+    }
+},
 
 //allow user to create new employee
-    async createEmployee(_,{EmployeeInput:{first_name, last_name, email, salary}}){
+    async createEmployee(_,{EmployeeInput:{first_name, last_name, email, salary , gender}}){
         const Employee = new employee({
-        first_name,
-        last_name,
-        email,
-        salary
+        first_name : first_name,
+        last_name  : last_name,
+        email : email,
+        salary: salary,
+        gender: gender
     })
+        const res = await Employee.save();
+
+        return {
+            id: res.id,
+            ...res.doc, //showing different fields of employee
+        }
 },
 //allow user to update employee
 async updateEmployee(_,{ID, EmployeeInput:{first_name, last_name, email, salary}}){
@@ -37,32 +43,33 @@ async updateEmployee(_,{ID, EmployeeInput:{first_name, last_name, email, salary}
 },
 
 //allow user to delete employee
-async deleteEmployee(_,{ID}){await employee.deleteOne({_id: ID}).deleteCount;
+async deleteEmployee(_,{ID}){
+    await employee.deleteOne({_id: ID}).deleteCount;
     return true;
 },
 
-//QUERY SECTION starts here 
-Query:{
-    //allow user to access system 
-    async getUser(_,{UserInput:{username, password,email}}){
-        const user = await user.find({username, password,email});
-        return user
-    },
-
-//select employee by ID 
-    async employeeID(_,{ID}){
-        return await employee.findById(ID);
-    },
-//get all employee
-    async viewEmployees(){
-        const employees = await employee.find();
-        return employees.map(employee => {
-            return {
-                id: employee.id,
-                ...employee.doc
-            }
-        })
-}
-}
+// // QUERY SECTION starts here 
+// Query:{
+//     //allow user to access system 
+//     async getUser(_,{UserInput:{username, password,email}}){
+//         const user = await user.find({username, password,email});
+//         return user
+//     },
+    
+// //select employee by ID 
+//     async employeeID(_,{ID}){
+//         return await employee.findById(ID);
+//     },
+// //get all employee
+//     async viewEmployees(){
+//         const employees = await employee.find();
+//         return employees.map(employee => {
+//             return {
+//                 id: employee.id,
+//                 ...employee.doc
+//             }
+//         })
+// }
+// }
 }
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -4,59 +4,58 @@ const { gql } = require('apollo-server')
 module.exports = gql`
 
 type User{
-    username: String!
-    email: String!
-    password: String!
+    username: String
+    email: String
+    password: String
 }
 
 type Employee{
-    first_name: String!
-    last_name: String!
-    email: String!
-    salary: Float!
-    gender: String!
+    first_name: String
+    last_name: String
+    email: String
+    salary: Float
+    gender: String
 }
 input UserInput{
-    username: String!
-    email: String!
-    password: String!
+    username: String
+    email: String
+    password: String
 }
 
 input EmployeeInput{
-    first_name: String!
-    last_name: String!
-    email: String!
-    salary: Float!
-    gender: String!
+    first_name: String
+    last_name: String
+    email: String
+    salary: Float
+    gender: String
 }
 
 input UserInputData{
-    username: String!
-    email: String!
-    password: String!
+    username: String
+    email: String
+    password: String
 }
 
 input EmployeeInputData{
-    first_name: String!
-    last_name: String!
-    email: String!
-    salary: Float!
-    gender: String!
+    first_name: String
+    last_name: String
+    email: String
+    salary: Float
+    gender: String
 }
 
 type Query{
-    getUser(username: String!, password: String!, email: String!): User!
-    employeeID(ID: ID!): Employee!
+    getUser(username: String, password: String, email: String!): User
+    employeeID(ID: ID!): Employee
     viewEmployees: [Employee!]!
-
 }
 
 
 type Mutation{
-    createUser(userInput: UserInputData): User!
-    createEmployee(employeeInput: EmployeeInputData): Employee!
-    updateEmployee(ID: ID!, employeeInput: EmployeeInput): Boolean!
-    deleteEmployee(ID: ID!): Boolean!
+    createUser(userInput: UserInputData): User
+    createEmployee(employeeInput: EmployeeInputData): Employee
+    updateEmployee(ID: ID!, employeeInput: EmployeeInputData): Boolean
+    deleteEmployee(ID: ID!): Boolean
 }`
 
 
